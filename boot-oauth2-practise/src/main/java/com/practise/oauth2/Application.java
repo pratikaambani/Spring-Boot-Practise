35f81a22992900a729dbcdaf9ee7b7d5e8669a10
Pratik <pratikaambani@gmail.com>
2017-10-07 17:52:42 +0530

    added spring boot OAuth2 practise example

---
+++ boot-oauth2-practise/src/main/java/com/practise/oauth2/Application.java
@@ -0,0 +1,19 @@
+package com.practise.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * Created by Pratik Ambani on 7/10/2017.
+ */
+@SpringBootApplication()
+@EnableAutoConfiguration()
+@EnableResourceServer
+public class Application {  
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ boot-oauth2-practise/src/main/java/com/practise/oauth2/controllers/ResourceController.java
@@ -0,0 +1,54 @@
+package com.practise.oauth2.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Pratik Ambani on 7/10/2017.
+ */
+@RestController
+@RequestMapping("/resources")
+public class ResourceController {
+
+    //http://localhost:8080/resources/user
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @RequestMapping(value = "user", method = RequestMethod.GET)
+    public String helloUser() {
+        return "hello user";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value = "admin", method = RequestMethod.GET)
+    public String helloAdmin() {
+        return "hello admin";
+    }
+
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @RequestMapping(value = "client", method = RequestMethod.GET)
+    public String helloClient() {
+        return "hello user authenticated by normal client";
+    }
+
+    @PreAuthorize("hasRole('ROLE_TRUSTED_CLIENT')")
+    @RequestMapping(value = "trusted_client", method = RequestMethod.GET)
+    public String helloTrustedClient() {
+        return "hello user authenticated by trusted client";
+    }
+
+    @RequestMapping(value = "principal", method = RequestMethod.GET)
+    public Object getPrincipal() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return principal;
+    }
+
+    @RequestMapping(value = "roles", method = RequestMethod.GET)
+    public Object getRoles() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+    }
+
+
+}
