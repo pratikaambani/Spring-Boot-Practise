38d6e45b6c907a225c49f75a0863c2d98d78244c
Pratik Ambani <pratikaambani@gmail.com>
2018-02-26 23:30:57 +0530

    added spring-boot-loader

---
+++ spring-boot-loader/src/main/java/com/practise/loader/SpringbootApplicationExample.java
@@ -0,0 +1,18 @@
+package com.practise.loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by Pratik Ambani on 26/02/2018.
+ */
+@SpringBootApplication
+public class SpringbootApplicationExample {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpringbootApplicationExample.class, args);
+        for (String name : applicationContext.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
+    }
+}
